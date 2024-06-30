d00bd81ffc2d3274c4626f86605161f90dd21378
Jorge Daniel Silva <jorgesilva@assertsolutions.com>
2024-06-30 12:18:49 -0300

    se agrega primer proyecto para alta de usuarios.

---
+++ clientes-crud/src/test/java/com/danielsilva/clientes_crud/ClientesCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danielsilva.clientes_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientesCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
